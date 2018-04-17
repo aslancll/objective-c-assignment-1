cde63c6932b3483d1f2cf22c087f4e655520356d
celalaslan <celalaslan@outlook.com>
2018-04-17 16:47:48 -0700

    do while loop and switch case loop implemented.

--- obj-c-assignment#1/obj-c-assignment#1/main.m
+++ obj-c-assignment#1/obj-c-assignment#1/main.m
@@ -8,11 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+int option;
+char inputChars[255];
+BOOL on = YES;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Please make a selection: \n1 - Uppercase\n2 - Lowercase\n3 - Numberize\n4 - Canadianize\n5 - Respond\n6 - De-Space-It");
         
+        NSLog(@"Please enter a string to manipulate: ");
+        fgets(inputChars, 255, stdin);
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        NSLog(@"\nYour input was %@\n\n", inputString);
+        
+        do{
+            printf("\nPlease make a selection from menu: \n\n\t  MENU\n\n1 - Uppercase\n2 - Lowercase\n3 - Numberize\n4 - Canadianize\n5 - Respond\n6 - De-Space-It\n7 - Exit\n");
+            scanf("%d",&option);
+            
+            switch (option) {
+                case 1:
+                    printf("Uppercase\n");
+                    break;
+                case 2:
+                    printf("Lowercase\n");
+                    break;
+                case 3:
+                    printf("Numberize\n");
+                    break;
+                case 4:
+                    printf("Canadianize\n");
+                    break;
+                case 5:
+                    printf("Respond\n");
+                    break;
+                case 6:
+                    printf("De-Space-It\n");
+                    break;
+                case 7:
+                    on = NO;
+                    printf("Exiting..\n");
+                    break;
+                default:
+                    printf("Wrong input.\n");
+            }
+        }while (on);
         
         
     }
